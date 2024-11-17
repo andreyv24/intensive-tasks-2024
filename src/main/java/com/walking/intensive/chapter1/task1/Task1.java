732b19b1cdea5973f9878badfd2ac3bf4ef1a92e
andreyv24 <midnait999@gmail.com>
2024-11-18 00:47:30 +0300

    .

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,7 +15,7 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 10;
+        int age = 1;
 
         System.out.println(getAgeString(age));
     }
