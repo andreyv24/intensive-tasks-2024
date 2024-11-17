87fc727ad5eb9d073527c53aaed4e6c0d5100eb2
andreyv24 <midnait999@gmail.com>
2024-11-18 00:23:09 +0300

    Решение через "if else", возможно, правильнее делать через switch case.

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,14 +15,29 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 5;
 
         System.out.println(getAgeString(age));
     }
-
     static String getAgeString(int age) {
-//        Место для вашего кода
+             // Проверка на отрицательные цифры, 0 и цифры более 122 (max установленный возраст человека)
+        if (age <= 0 || age >= 122 ) {
+            return "Некорректный ввод";
+        }
+             // Поиск чисел относящихся к значению "лет"
+        else if ( age == 11 || age % 10 == 5 || age % 100 == 5 || age % 10 == 6 || age % 100 == 6 || age % 10 == 7 || age % 100 == 7 || age % 10 == 8 || age % 100 == 8 || age % 10 == 9 || age % 100 == 9 || age % 10 == 0)  {
+            return (age + " лет");
+        }
+            // Поиск чисел относящихся к значению "год"
+         else if (age % 10 == 1 || age % 100 == 1)  {
+            return (age + " год");
+        }
+            // Поиск чисел относящихся к значению "года", они же все оставшиеся
+        // else if (age % 10 == 2 || age % 100 == 2 || age % 10 == 3 || age % 100 == 3 || age % 10 == 4 || age % 100 == 4)  {
+         else
+             return (age + " года");
+        // }
 
-        return null; // Заглушка. При реализации - удалить
     }
 }
+
