e43581ecd8c08d1dc4e2f5643f96517b8c19927a
andreyv24 <midnait999@gmail.com>
2024-11-20 01:52:10 +0300

    .

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -23,19 +23,31 @@ package com.walking.intensive.chapter1.task4;
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
 public class Task4 {
-    public static void main(String[] args) {
+
+     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
+        double a = 5;
+        double b = -4;
+        double c = -1;
 
         System.out.println(solveEquation(a, b, c));
-
     }
-
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        double d = b * b - 4 * a * c;
+        double x = -1 * b / 2 * a;
+        double x1 = (-1 * b + Math.sqrt(d)) / (2 * a);
+        double x2 = (-1 * b - Math.sqrt(d))/ (2 * a);
 
-        return null; // Заглушка. При реализации - удалить
+        if (d == 0) {
+            return "Корень: " + x;
+        }
+        if (d > 0) {
+            if (x1 > x2) {
+               return "Корни: " + x1 + ";" + x2;
+            }
+            else
+                return "Корни: " + x2 + ";" + x1;
+        }
+        return "Уравнение не имеет корней";
     }
 }
