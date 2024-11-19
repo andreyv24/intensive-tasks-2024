437dcdecda7ea9d6a772be7b8c98d1f6144f17ef
andreyv24 <midnait999@gmail.com>
2024-11-19 22:09:49 +0300

    Исправил неправильную работу кода при вводе чисел 11, 12, 13, 14.

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,7 +15,7 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 224;
         System.out.println(getAgeString(age));
     }
     static String getAgeString(int age) {
@@ -36,11 +36,11 @@ public class Task1 {
             return ("Вам " + age + " год");
         }
             // Поиск чисел относящихся к значению "года"
-        else if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4)  {
+//        else if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4)  {
             return ("Вам " + age + " года");
         }
-        return "";
+//        return "";
     }
 
-    }
+//    }
 
