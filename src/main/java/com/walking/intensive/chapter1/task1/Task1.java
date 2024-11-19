66acdae1f3f1264bc99bc1413d6eb12c4036f6ea
andreyv24 <midnait999@gmail.com>
2024-11-19 23:26:19 +0300

    добавил условие возраст <=127; убрал лишние скобки; убрал else.

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,34 +15,32 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 111;
+        int age = 126;
         System.out.println(getAgeString(age));
     }
     static String getAgeString(int age) {
              // Проверка на отрицательные цифры и 0)
-        if (age <= 0) {
+        if (age <= 0 || age >= 127 ) {
             return "Некорректный ввод";
         }
         // Поиск чисел типа 11,12,13,14, и т.д.
-        // else if (age % 100 == 11 ||  age % 100 == 12 || age % 100 == 13 ||  age % 100 == 14)  {
-        else if (age % 100 >= 11 &&  age % 100 <= 14)  {
-            return ("Вам " + age + " лет");
+        if (age % 100 >= 11 &&  age % 100 <= 14)  {
+            return "Вам " + age + " лет";
         }
              // Поиск чисел относящихся к значению "лет"
-//        else if ( age % 10 == 5 || age % 10 == 6 ||  age % 10 == 7 || age % 10 == 8 ||  age % 10 == 9 || age % 10 == 0)  {
-        else if (age % 10 == 0 || age % 10 >= 5 && age % 10 <= 9)  {
-            return ("Вам " + age + " лет");
+        if (age % 10 == 0 || age % 10 >= 5 && age % 10 <= 9)  {
+            return "Вам " + age + " лет";
         }
             // Поиск чисел относящихся к значению "год"
-         else if (age % 10 == 1)  {
+        if (age % 10 == 1)  {
             return ("Вам " + age + " год");
         }
             // Поиск чисел относящихся к значению "года"
-//        else if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4)  {
-            return ("Вам " + age + " года");
+        if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4)  {
+            return "Вам " + age + " года";
         }
-//        return "";
+        return "";
     }
 
-//    }
+    }
 
