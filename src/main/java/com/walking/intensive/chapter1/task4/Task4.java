70b7e4645eea0abb05b70ff8d0251414be3902c8
andreyv24 <midnait999@gmail.com>
2024-11-21 01:12:55 +0300

    .

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -26,28 +26,38 @@ public class Task4 {
 
      public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 5;
-        double b = -4;
-        double c = -1;
+        double a = 0;
+        double b = 0;
+        double c = 4;
 
         System.out.println(solveEquation(a, b, c));
     }
     static String solveEquation(double a, double b, double c) {
         double d = b * b - 4 * a * c;
         double x = -1 * b / 2 * a;
-        double x1 = (-1 * b + Math.sqrt(d)) / (2 * a);
+        double x0 = -1 * c / b;
+        double x1 = (-1 * b + Math.sqrt(d))/ (2 * a);
         double x2 = (-1 * b - Math.sqrt(d))/ (2 * a);
 
-        if (d == 0) {
-            return "Корень: " + x;
+        if (a == 0 && b == 0 && c == 0 ) {
+        return "Бесконечное множество решений.";
+        }
+        if (a == 0 && b == 0) {
+            return "Количество решений: 0.";
+        }
+        if (a == 0) {
+            return "Количество решений: 1. Корень: " + (int)(x0);
+        }
+        if (d == 0 ) {
+            return "Количество решений: 1. Корень: " + (int)(x);
         }
         if (d > 0) {
-            if (x1 > x2) {
-               return "Корни: " + x1 + ";" + x2;
-            }
-            else
-                return "Корни: " + x2 + ";" + x1;
+        if (x1 < x2) {
+        return "Количество решений: 2. Корни: " + (int)(x1) + ";" + (int)(x2);
         }
-        return "Уравнение не имеет корней";
+        else
+        return "Количество решений: 2. Корни: " + (int)(x2) + ";" + (int)(x1);
+        }
+        return "Количество решений: 0.";
     }
 }
