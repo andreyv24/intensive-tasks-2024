e2dc57766a835f980232f0a0627fb953990c4d50
andreyv24 <midnait999@gmail.com>
2024-12-03 20:35:25 +0300

    1. улучшил читаемость кода. 2. убрал ненужное else. 3. заменил if на Math.min и Math.max. 4. убрал лишние вычисления.

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -26,42 +26,45 @@ public class Task4 {
 
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-//        double a = 1;
-//        double b = 5;
-//        double c = 5;
-//        System.out.println(solveEquation(a, b, c));
     }
 
     static String convertToString(double a) {
-        String answer = String.valueOf((int)(a));
+        String answer = String.valueOf((int) (a));
+
         return answer;
     }
 
     static String solveEquation(double a, double b, double c) {
-        double d = b * b - 4 * a * c;
-        double x = -1 * b / 2 * a;
-        double x0 = -1 * c / b;
-        double x1 = (-1 * b + Math.sqrt(d)) / (2 * a);
-        double x2 = (-1 * b - Math.sqrt(d)) / (2 * a);
 
         if (a == 0 && b == 0 && c == 0) {
+
             return "Бесконечное множество решений.";
         }
         if (a == 0 && b == 0) {
+
             return "Количество решений: 0.";
         }
+
         if (a == 0) {
+            double x0 = -1 * c / b;
+
             return "Количество решений: 1. Корень: " + convertToString(x0);
         }
+        double d = b * b - 4 * a * c;
+
         if (d == 0) {
+            double x = -1 * b / 2 * a;
+
             return "Количество решений: 1. Корень: " + convertToString(x);
         }
+
         if (d > 0) {
-            if (x1 < x2) {
-                return "Количество решений: 2. Корни: " + convertToString(x1) + ";" + convertToString(x2);
-            } else
-                return "Количество решений: 2. Корни: " + convertToString(x2) + ";" + convertToString(x1);
+            double x1 = (-1 * b + Math.sqrt(d)) / (2 * a);
+            double x2 = (-1 * b - Math.sqrt(d)) / (2 * a);
+
+            return "Количество решений: 2. Корни: " + convertToString(Math.min(x1, x2)) + ";" + convertToString(Math.max(x1, x2));
         }
+
         return "Количество решений: 0.";
     }
 }
