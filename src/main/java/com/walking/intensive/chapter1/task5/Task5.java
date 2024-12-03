e563335f52e50cf0f038ca35cc3dcd4d819b0876
andreyv24 <midnait999@gmail.com>
2024-12-03 19:31:53 +0300

    1.Добавил скобки ко всем if. 2.Убрал лишние скобки. 3.Разделил блоки пробелами.

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -16,7 +16,7 @@ public class Task5 {
     }
 
     static boolean ifTriangleExist(double a, double b, double c) {
-        return  ((a + b) > c && (a + c) > b && (b + c) > a);
+        return ((a + b) > c && (a + c) > b && (b + c) > a);
     }
 
     static double getTrianglePerimeter(double a, double b, double c) {
@@ -42,7 +42,6 @@ public class Task5 {
             return -1;
         }
         return getTriangleSquare(a, b, c);
-
     }
 
     /**
@@ -54,20 +53,21 @@ public class Task5 {
      */
     static double[] getHeights(double a, double b, double c) {
         double[] heights = new double[3];
-        if (!ifTriangleExist(a, b, c))
+        if (!ifTriangleExist(a, b, c)) {
             return new double[]{};
-        {
-            double square = getTriangleSquare(a, b, c);
-            double heightA = (2 * square) / a;
-            double heightB = (2 * square) / b;
-            double heightC = (2 * square) / c;
-
-            heights[0] = heightA;
-            heights[1] = heightB;
-            heights[2] = heightC;
-            Arrays.sort(heights);
-            return heights;
         }
+
+        double square = getTriangleSquare(a, b, c);
+        double heightA = (2 * square) / a;
+        double heightB = (2 * square) / b;
+        double heightC = (2 * square) / c;
+
+        heights[0] = heightA;
+        heights[1] = heightB;
+        heights[2] = heightC;
+        Arrays.sort(heights);
+
+        return heights;
     }
 
     /**
@@ -79,20 +79,20 @@ public class Task5 {
      */
     static double[] getMedians(double a, double b, double c) {
         double[] medians = new double[3];
-        if (!ifTriangleExist(a, b, c))
+        if (!ifTriangleExist(a, b, c)) {
             return new double[]{};
-        {
-            double medianA = 0.5 * (Math.sqrt((2 * b * b) + (2 * c * c) - (a * a)));
-            double medianB = 0.5 * (Math.sqrt((2 * a * a) + (2 * c * c) - (b * b)));
-            double medianC = 0.5 * (Math.sqrt((2 * a * a) + (2 * b * b) - (c * c)));
-
-            medians[0] = medianA;
-            medians[1] = medianB;
-            medians[2] = medianC;
-            Arrays.sort(medians);
-
-            return medians;
         }
+
+        double medianA = 0.5 * (Math.sqrt((2 * b * b) + (2 * c * c) - (a * a)));
+        double medianB = 0.5 * (Math.sqrt((2 * a * a) + (2 * c * c) - (b * b)));
+        double medianC = 0.5 * (Math.sqrt((2 * a * a) + (2 * b * b) - (c * c)));
+
+        medians[0] = medianA;
+        medians[1] = medianB;
+        medians[2] = medianC;
+        Arrays.sort(medians);
+
+        return medians;
     }
 
     /**
@@ -104,19 +104,20 @@ public class Task5 {
      */
     static double[] getBisectors(double a, double b, double c) {
         double[] bisectors = new double[3];
-        if (!ifTriangleExist(a, b, c))
+        if (!ifTriangleExist(a, b, c)) {
             return new double[]{};
-        {
-            double bisectorA = (Math.sqrt((b * c) * (b + c + a) * (b + c - a))) / (b + c);
-            double bisectorB = (Math.sqrt((a * c) * (a + c + b) * (a + c - b))) / (a + c);
-            double bisectorC = (Math.sqrt((a * b) * (a + b + c) * (a + b - c))) / (a + b);
-
-            bisectors[0] = bisectorA;
-            bisectors[1] = bisectorB;
-            bisectors[2] = bisectorC;
-            Arrays.sort(bisectors);
-            return bisectors;
         }
+
+        double bisectorA = (Math.sqrt((b * c) * (b + c + a) * (b + c - a))) / (b + c);
+        double bisectorB = (Math.sqrt((a * c) * (a + c + b) * (a + c - b))) / (a + c);
+        double bisectorC = (Math.sqrt((a * b) * (a + b + c) * (a + b - c))) / (a + b);
+
+        bisectors[0] = bisectorA;
+        bisectors[1] = bisectorB;
+        bisectors[2] = bisectorC;
+        Arrays.sort(bisectors);
+
+        return bisectors;
     }
 
     /**
@@ -128,20 +129,20 @@ public class Task5 {
      */
     static double[] getAngles(double a, double b, double c) {
         double[] angles = new double[3];
-        if (!ifTriangleExist(a, b, c))
+        if (!ifTriangleExist(a, b, c)) {
             return new double[]{};
-        {
-            double cosA = ((b * b) + (c * c) - (a * a)) / (2 * b * c);
-            double cosB = ((a * a) + (c * c) - (b * b)) / (2 * a * c);
-            double cosG = ((a * a) + (b * b) - (c * c)) / (2 * a * b);
-
-            angles[0] = Math.toDegrees(Math.acos(cosA));
-            angles[1] = Math.toDegrees(Math.acos(cosB));
-            angles[2] = Math.toDegrees(Math.acos(cosG));
-            Arrays.sort(angles);
-
-            return angles;
         }
+
+        double cosA = ((b * b) + (c * c) - (a * a)) / (2 * b * c);
+        double cosB = ((a * a) + (c * c) - (b * b)) / (2 * a * c);
+        double cosG = ((a * a) + (b * b) - (c * c)) / (2 * a * b);
+
+        angles[0] = Math.toDegrees(Math.acos(cosA));
+        angles[1] = Math.toDegrees(Math.acos(cosB));
+        angles[2] = Math.toDegrees(Math.acos(cosG));
+        Arrays.sort(angles);
+
+        return angles;
     }
 
     /**
@@ -152,15 +153,15 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getInscribedCircleRadius(double a, double b, double c) {
-        if (!ifTriangleExist(a, b, c))
+        if (!ifTriangleExist(a, b, c)) {
             return -1;
-        {
-            double perimeter = getTrianglePerimeter(a, b, c);
-            double square = getTriangleSquare(a, b, c);
-            double rIntRadius = square / perimeter;
-
-            return rIntRadius;
         }
+
+        double perimeter = getTrianglePerimeter(a, b, c);
+        double square = getTriangleSquare(a, b, c);
+        double rIntRadius = square / perimeter;
+
+        return rIntRadius;
     }
 
     /**
@@ -171,14 +172,16 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getCircumradius(double a, double b, double c) {
-        if (!ifTriangleExist(a, b, c))
+        if (!ifTriangleExist(a, b, c)) {
             return -1;
-        {
-            double square = getTriangleSquare(a, b, c);
-            double rOutRadius = (a * b * c) / (4 * square);
-            return rOutRadius;
         }
+
+        double square = getTriangleSquare(a, b, c);
+        double rOutRadius = (a * b * c) / (4 * square);
+
+        return rOutRadius;
     }
+
     /**
      * Дополнительная задача по желанию.
      *
@@ -194,12 +197,12 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getAreaAdvanced(double a, double b, double c) {
-        if (!ifTriangleExist(a, b, c))
+        if (!ifTriangleExist(a, b, c)) {
             return -1;
-        {
-            double cosA = (a * a + b * b - c * c) / (2 * a * b);
-            double square = (0.5 * a * b) * Math.sqrt((1 - (cosA * cosA)));
-            return square;
         }
+
+        double cosA = (a * a + b * b - c * c) / (2 * a * b);
+        double square = (0.5 * a * b) * Math.sqrt((1 - (cosA * cosA)));
+        return square;
     }
 }
