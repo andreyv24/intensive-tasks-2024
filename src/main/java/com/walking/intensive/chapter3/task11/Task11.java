0b1f891c460fdbf8fdfbcae703152ec0301bb77e
andreyv24 <midnait999@gmail.com>
2024-12-17 20:22:53 +0300

    .

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -42,11 +42,40 @@ package com.walking.intensive.chapter3.task11;
  */
 public class Task11 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+//        int[] inputArray1 = new int[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+//        int[] inputArray2 = new int[] {1, 2, 4, 0, 1, 10, 20};
+//        int[] inputArray3 = new int[] {10, 9, 8, 7, 6, 5};
+//        int[] inputArray4 = new int[] {1, 4 ,2 , 5, 3};
+//        int[] inputArray5 = new int[] {3, 0, 1, 2,};
+//        int[] inputArray6 = new int[] {1, 4 ,2};
+//        getOddSubArraysElementsSum(inputArray1);
+//        System.out.println("Сумма элементов всех возможных подмассивов нечетной длины по массиву 1 = " + getOddSubArraysElementsSum(inputArray1));
+//        getOddSubArraysElementsSum(inputArray2);
+//        System.out.println("Сумма элементов всех возможных подмассивов нечетной длины  по массиву 2 = " + getOddSubArraysElementsSum(inputArray2));
+//        getOddSubArraysElementsSum(inputArray3);
+//        System.out.println("Сумма элементов всех возможных подмассивов нечетной длины  по массиву 3 = " + getOddSubArraysElementsSum(inputArray3));
+//        getOddSubArraysElementsSum(inputArray4);
+//        System.out.println("Сумма элементов всех возможных подмассивов нечетной длины  по массиву 4 = " + getOddSubArraysElementsSum(inputArray4));
+//        getOddSubArraysElementsSum(inputArray5);
+//        System.out.println("Сумма элементов всех возможных подмассивов нечетной длины  по массиву 5 = " + getOddSubArraysElementsSum(inputArray5));
+//        getOddSubArraysElementsSum(inputArray6);
+//        System.out.println("Сумма элементов всех возможных подмассивов нечетной длины  по массиву 6 = " + getOddSubArraysElementsSum(inputArray6));
     }
 
     static int getOddSubArraysElementsSum(int[] array) {
-        // Ваш код
-        return 0;
+        int sumElementsSubarray = 0;
+        int i = 0;
+        while (i < array.length) {
+            if (array[i] < 0) {
+                return -1;
+            }
+
+            int elementCounterInOddSubarrays = 0;
+            int elementCounterInSubarrays = ((array.length - i) * (i + 1));
+            elementCounterInOddSubarrays = (elementCounterInSubarrays + 1) / 2;
+            sumElementsSubarray = sumElementsSubarray + elementCounterInOddSubarrays * array[i];
+            i++;
+        }
+        return sumElementsSubarray;
     }
 }
