c7c0b0aa663247a2fcae3ad8d0922d53448106b7
andreyv24 <midnait999@gmail.com>
2024-12-19 03:18:20 +0300

    .

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,8 +29,9 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        }
+        return year % 400 == 0;
     }
 }
