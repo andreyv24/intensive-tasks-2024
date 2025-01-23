0d44f430fda370b024e74fa6919ea9bc1aae01bb
andreyv24 <midnait999@gmail.com>
2025-01-23 03:17:34 +0300

    Изначально задача была решена перебором и сравнением сумм цифр через два цикла от 0 до 999. getHappyTicketChanceVer1 - данное решение не было засчитано. Предлагается несколько более быстрых способов подсчета счастливых билетов.
    getHappyTicketChanceVer2 – пять циклов, от 0 до 9 и определение шестой цифры через пять первых.
    
    getHappyTicketChanceVer3 – четыре цикла,  исходя из того, что можно посчитать суммы трёх цифр, возвести в «квадрат» чтобы получить общее число комбинаций, и не считать суммы от 0 до 27 (9+9+9) а только половину - суммы от 0 до 13 и в конце удвоить получившееся значение.
    
    getHappyTicketChanceVer4 – два цикла, считаем опять только половину сумм от 0 до 13 и исходим из того, что сумма трёх чисел определяется числом вариаций второй цифры, заданной через сумму и первую цифру.

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -20,14 +20,39 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        System.out.println("\n");
+        long startTimeV1 = System.currentTimeMillis();
+        getHappyTicketChanceVer1();
+        long endtTimeV1 = System.currentTimeMillis();
+        System.out.println("1. Время выполнения версии с 2-мя циклами от 0 до 999 = " + (endtTimeV1 - startTimeV1) + "мс");
+        System.out.println("\n");
+
+        long startTimeV2 = System.currentTimeMillis();
+        getHappyTicketChanceVer2();
+        long endtTimeV2 = System.currentTimeMillis();
+        System.out.println("2. Время выполнения версии с 5-ью циклами от 0 до 9 = " + (endtTimeV2 - startTimeV2) + "мс");
+        System.out.println("\n");
+
+        long startTimeV3 = System.currentTimeMillis();
+        getHappyTicketChanceVer3();
+        long endtTimeV3 = System.currentTimeMillis();
+        System.out.println("3. Время выполнения версии с 4-мя циклами = " + (endtTimeV3 - startTimeV3) + "мс");
+        System.out.println("\n");
+
+        long startTimeV4 = System.currentTimeMillis();
+        getHappyTicketChanceVer4();
+        long endtTimeV4 = System.currentTimeMillis();
+        System.out.println("4. Время выполнения с 2-мя циклами  = " + (endtTimeV4 - startTimeV4) + "мс");
+        System.out.println("\n");
     }
 
-    static double getHappyTicketChance() {
+    //   Перебор через 2 цикла от 0 до 999
+    static double getHappyTicketChanceVer1() {
         int leftDigits = 0;
         int rightDigits = 0;
-        double getEquality = 0;
-        double getProbability;
+        double Equality = 0;
+        double Probability;
 
         for (int i = 0; i < 1000; i++) {
             if (i < 10) {
@@ -37,11 +62,11 @@ public class Task8 {
             } else {
                 leftDigits = i / 100 + (i % 100) / 10 + (i % 10);
             }
+
             for (int j = 1; j < 1000; j++) {
                 if (leftDigits == rightDigits) {
-                    getEquality++;
+                    Equality++;
                 }
-
                 if (j < 10) {
                     rightDigits = (j % 10);
                 } else if (j < 99) {
@@ -49,10 +74,75 @@ public class Task8 {
                 } else {
                     rightDigits = j / 100 + (j % 100) / 10 + (j % 10);
                 }
-
             }
         }
-        System.out.println(getEquality / (1000000));
-        return getProbability = getEquality / (1000000);
+
+        System.out.println("Вероятность счастливого билета = " + Equality / (1000000));
+        return Probability = Equality / (1000000);
+    }
+
+    //   Перебор через 5 циклов от 0 до 9
+    static double getHappyTicketChanceVer2() {
+        double Equality = 0;
+        double Probability;
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
+                        for (int m = 0; m < 10; m++) {
+                            int n = i + j + k - (l + m);
+                            if (n >= 0 && n < 10)
+                                Equality++;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("Вероятность счастливого билета = " + Equality / (1000000));
+        return Probability = Equality / (1000000);
+    }
+
+    //   Подсчёт через 4 цикла
+    static double getHappyTicketChanceVer3() {
+        double luckyTiketsNumber = 0;
+        double Probability;
+
+        for (int i = 0; i < 14; i++) {
+            int luckyTiketsNumberInOneAmount = 0;
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
+                        if (i == j + k + l) {
+                            luckyTiketsNumberInOneAmount += 1;
+                        }
+                    }
+                }
+            }
+            luckyTiketsNumber += luckyTiketsNumberInOneAmount * luckyTiketsNumberInOneAmount;
+        }
+        luckyTiketsNumber *= 2;
+
+        System.out.println("Вероятность счастливого билета = " + luckyTiketsNumber / (1000000));
+        return Probability = luckyTiketsNumber / (1000000);
+    }
+
+    //   Оптимизированная версия
+    static double getHappyTicketChanceVer4() {
+        double luckyTiketsNumber = 0;
+        double Probability;
+
+        for (int i = 0; i < 14; i++) {
+            int luckyTiketsNumberInOneAmount = 0;
+            for (int j = 0 ; j < Math.min(10, i + 1); j++) {
+                luckyTiketsNumberInOneAmount += Math.min(9, i - j) - Math.max(0, i - j - 9) + 1;
+            }
+            luckyTiketsNumber += luckyTiketsNumberInOneAmount * luckyTiketsNumberInOneAmount;
+        }
+        luckyTiketsNumber *= 2;
+
+        System.out.println("Вероятность счастливого билета = " + luckyTiketsNumber / (1000000));
+        return Probability = luckyTiketsNumber / (1000000);
     }
 }
